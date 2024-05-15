d63e685b0783c6d6ba6f2181d0cbcf31775f9226
prakrutivaghasiya <prakrutivaghasiya1@gmail.com>
2024-05-15 17:25:20 -0300

    Final touches before deploying

--- backend/server.js
+++ backend/server.js
@@ -25,10 +25,6 @@ app.use(express.urlencoded({extended: true}));
 // Cookie Parser Middlerware
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-    res.send('API is running...');
-})
-
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
@@ -38,6 +34,18 @@ app.get("/api/config/paypal", (req,res) => res.send({clientId: process.env.PAYPA
 
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, '/uploads')));
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/ui/build')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'ui', 'build', 'index.html'))
+    });
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running....');
+    })
+}
 
 app.use(notFound);
 app.use(errorHandler);
--- ui/src/components/ProductCarousel.jsx
+++ ui/src/components/ProductCarousel.jsx
@@ -10,11 +10,11 @@ const ProductCarousel = () => {
     const {data: products, isLoading, error} = useGetTopProductsQuery();
 
   return isLoading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : (
-    <Carousel pause='hover' className='bg-primary mb-4' style={{minHeight: '150px'}}>
+    <Carousel pause='hover' className='bg-primary mb-4' style={{minHeight: '200px'}}>
         {products.map((product) => (
             <Carousel.Item key={product._id}>
                 <Link to={`/product/${product._id}`}>
-                    <Image src={product.image} alt={product.name} fluid style={{ minHeight: '150px', height: '15vw', width: 'auto', objectFit: 'cover'}}/>
+                    <Image src={product.image} alt={product.name} fluid style={{ minHeight: '200px', height: '25vw', width: 'auto', objectFit: 'cover'}}/>
                     <Carousel.Caption className='carousel-caption'>
                         <h5>{product.name} (${product.price})</h5>
                     </Carousel.Caption>
