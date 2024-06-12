2c25b70ebe469110776a68296e0b3f8a5450f24f
prakrutivaghasiya <prakrutivaghasiya1@gmail.com>
2024-06-12 17:28:09 -0300

    Fix for build error

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.use("/api/upload", uploadRoute);
 
 app.get("/api/config/paypal", (req,res) => res.send({clientId: process.env.PAYPAL_CLIENT_ID}));
 
-// const __dirname = path.resolve();
+const __dirname = path.resolve();
 // app.use("/uploads", express.static(path.join(__dirname, '/uploads')));
 
 if(process.env.NODE_ENV === 'production') {
